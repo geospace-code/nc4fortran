dc9e869dd76b093f1013af032a21938105999995
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-23 21:51:49 -0500

    test add read variable slice test

--- src/tests/test_io.f90
+++ src/tests/test_io.f90
@@ -26,10 +26,10 @@ contains
 subroutine test_real_int(path)
 
 character(*), intent(in) :: path
-type(netcdf_file) :: ncf
+type(netcdf_file) :: hf
 
 integer :: i1(4), ierr
-real(real32)    :: nan, r1(4), r2(4,4)
+real(real32)    :: nan, r1(4), r2(4,4), B(6,6)
 integer(int32), dimension(4,4) :: i2, i2t
 integer(int64), dimension(4,4) :: i2t64
 real(real32), allocatable :: rr2(:,:)
@@ -53,45 +53,49 @@ enddo
 
 r2 = i2
 
-call ncf%initialize(path // '/io_test.nc', ierr, status='replace', action='rw', comp_lvl=1)
+call hf%initialize(path // '/io_test.nc', ierr, status='replace', action='rw', comp_lvl=1)
 if(ierr/=0) error stop 'initialize'
 
-call ncf%write('nan', nan, ierr)
+call hf%write('nan', nan, ierr)
 if(ierr/=0) error stop 'write 0-D: real32 NaN'
 
-call ncf%write('real32-2d', r2, ierr, ['x', 'y'])
+call hf%write('real32-2d', r2, ierr, ['x', 'y'])
 if(ierr/=0) error stop 'write 2-D: real32'
 
-call ncf%write('real64-2d', real(r2, real64), ierr)
+call hf%write('real64-2d', real(r2, real64), ierr)
 if(ierr/=0) error stop 'write 2-D: real64'
 
-call ncf%write('int32-2d', i2, ierr, ['x', 'y'])
+call hf%write('int32-2d', i2, ierr, ['x', 'y'])
 if(ierr/=0) error stop 'write 2-D: int32'
 
-call ncf%write('int64-2d', int(i2, int64), ierr)
+call hf%write('int64-2d', int(i2, int64), ierr)
 if(ierr/=0) error stop 'write 2-D: int64'
 
-call ncf%finalize(ierr)
+call hf%finalize(ierr)
 if (ierr /= 0) error stop 'write finalize'
 
 !-----------------
 
-call ncf%initialize(path // '/io_test.nc', ierr,status='old',action='r')
+call hf%initialize(path // '/io_test.nc', ierr,status='old',action='r')
 
-call ncf%read('int32-2d',i2t, ierr)
+call hf%read('int32-2d',i2t, ierr)
 if (.not.all(i2==i2t)) error stop 'read 2-D: int32 does not match write'
 
-call ncf%read('int64-2d',i2t64, ierr)
+call hf%read('int64-2d',i2t64, ierr)
 if (.not.all(i2==i2t64)) error stop 'read 2-D: int64 does not match write'
 
-call ncf%shape('real32-2d', dimnames, dims, ierr)
+call hf%shape('real32-2d', dimnames, dims, ierr)
 allocate(rr2(dims(1), dims(2)))
-call ncf%read('real32-2d',rr2, ierr)
+call hf%read('real32-2d',rr2, ierr)
 if (.not.all(r2 == rr2)) error stop 'real 2-D: read does not match write'
 
- call ncf%read('nan',nant, ierr)
- if (.not.ieee_is_nan(nant)) error stop 'failed storing or reading NaN'
-call ncf%finalize(ierr)
+! check read into a variable slice
+call hf%read('real32-2d', B(2:5,3:6), ierr)
+if(.not.all(B(2:5,3:6) == r2)) error stop 'real 2D: reading into variable slice'
+
+call hf%read('nan',nant, ierr)
+if (.not.ieee_is_nan(nant)) error stop 'failed storing or reading NaN'
+call hf%finalize(ierr)
 if (ierr /= 0) error stop 'read finalize'
 
 end subroutine test_real_int
