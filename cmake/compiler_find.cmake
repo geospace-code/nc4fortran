675861450f6c358937e5a8a4890fb3a1f6518b7c
Michael Hirsch <scivision@users.noreply.github.com>
2021-01-31 01:34:35 -0500

    Cmake >= 3.13

--- cmake/compiler_find.cmake
+++ cmake/compiler_find.cmake
@@ -6,38 +6,43 @@ endif()
 
 set(CMAKE_CONFIGURATION_TYPES "Release;RelWithDebInfo;Debug" CACHE STRING "Build type selections" FORCE)
 
-# Help CMake find matching compilers, especially needed for MacOS
+# --- Help CMake find matching compilers, especially needed for MacOS
 
+set(_paths)
 if(APPLE)
+  # CMAKE_SYSTEM_NAME is not set till project()
   set(_paths /usr/local/bin /opt/homebrew/bin)
   # for Homebrew that's not on PATH (can be an issue on CI)
-else()
-  set(_paths)
 endif()
 
 
 function(find_fortran)
 
-if(NOT DEFINED ENV{FC})
-  # temporarily removed ifort, because Intel oneAPI release Dec 8, 2020 is broken for HDF5 and MPI in general.
-  find_program(FC
-    NAMES gfortran gfortran-11 gfortran-10 gfortran-9 gfortran-8 gfortran-7
-    PATHS ${_paths})
-  if(FC)
-    set(ENV{FC} ${FC})
-  endif()
+if(NOT DEFINED FC AND DEFINED ENV{FC})
+  set(FC $ENV{FC})
 endif()
 
-if(NOT DEFINED ENV{FC})
-  return()
+if(FC)
+  get_filename_component(_dir ${FC} DIRECTORY)
 endif()
-# ensure FC exists as a executable program
+# determine if the user is intending to use Intel oneAPI or default Gfortran
+# Need to check ifort because MKLROOT may be defined for
+# use of oneMKL with Gfortran on MacOS and Linux.
+if(DEFINED ENV{MKLROOT} OR FC MATCHES ".*ifort")
+  find_program(FC
+    NAMES ifort
+    HINTS ${_dir})
+endif()
+
 find_program(FC
-  NAMES $ENV{FC}
+  NAMES gfortran gfortran-12 gfortran-11 gfortran-10 gfortran-9 gfortran-8 gfortran-7
+  NAMES_PER_DIR
+  HINTS ${_dir}
   PATHS ${_paths})
 
 if(FC)
-  set(FC ${FC} PARENT_SCOPE)
+  set(ENV{FC} ${FC})
+  # ENV{FC} is how project() picks up our hint
 endif()
 
 endfunction(find_fortran)
@@ -45,44 +50,58 @@ endfunction(find_fortran)
 
 function(find_c)
 
-if(NOT FC AND DEFINED ENV{FC})
-  set(FC $ENV{FC})
+if(NOT DEFINED CC)
+  if(DEFINED ENV{CC})
+    set(CC $ENV{CC})
+  elseif(NOT DEFINED FC AND DEFINED ENV{FC})
+    set(FC $ENV{FC})
+  endif()
 endif()
 
-message(VERBOSE " FC hint: ${FC}")
-
-if(DEFINED ENV{CC})
-  set(_name $ENV{CC})
-  set(_dir)
-else()
+if(NOT DEFINED CC)
   # remember, Apple has "/usr/bin/gcc" which is really clang
   # the technique below is NECESSARY to work on Mac and not find the wrong GCC
-  if(FC)
+  if(DEFINED FC)
     get_filename_component(_dir ${FC} DIRECTORY)
   endif()
   # use same compiler for C and Fortran, which CMake might not do itself
   if(FC MATCHES ".*ifort")
     if(WIN32)
-      set(_name icl)
+      set(_cc icl)
     else()
-      set(_name icc)
+      set(_cc icc)
     endif()
   elseif(FC MATCHES ".*gfortran")
-    set(_name gcc gcc-11 gcc-10 gcc-9 gcc-8 gcc-7)
-  else()
-    return()
+    # get same GCC version as Gfortran
+    execute_process(COMMAND ${FC} -dumpversion
+      OUTPUT_VARIABLE _v
+      RESULT_VARIABLE _err)
+    if(NOT _err EQUAL 0)
+      return()
+    endif()
+    string(REGEX MATCH "^([0-9]+)" _v ${_v})
+    if(_v)
+      set(_cc gcc-${_v})
+    else()
+      set(_cc gcc-12 gcc-11 gcc-10 gcc-9 gcc-8 gcc-7 gcc)  # generic last to avoid AppleClang
+    endif()
   endif()
 endif()
 
+if(NOT _cc)
+  return()
+endif()
+
 find_program(CC
-  NAMES ${_name}
+  NAMES ${_cc}
+  NAMES_PER_DIR
+  PATHS ${_paths}  # PATHS are searched last
   HINTS ${_dir}
   NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
 
 if(CC)
-  set(CC ${CC} PARENT_SCOPE)
   set(ENV{CC} ${CC})
-  message(VERBOSE " CC hint: ${CC}")
+  # ENV{CC} is how project() picks up our hint
 endif()
 
 endfunction(find_c)
