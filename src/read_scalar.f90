5a0508cdd1b9d9ddbef1c8949ddabd05c8e972fe
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-27 19:01:18 -0400

    correct character read segfault seen with GCC 12

--- src/read_scalar.f90
+++ src/read_scalar.f90
@@ -6,26 +6,30 @@ contains
 
 
 module procedure nc_read_scalar
-integer :: varid, ier, i
-integer, allocatable :: dims(:)
+integer :: varid, ier, i, dimids(1)
+integer :: dims
+character(:), allocatable :: buf
 
-if(.not.self%is_open) error stop 'nc4fortran:reader file handle not open'
+if(.not.self%is_open) error stop 'ERROR:nc4fortran:reader file handle not open'
 
 ier = nf90_inq_varid(self%ncid, dname, varid)
+if (check_error(ier, dname)) error stop 'ERROR:nc4fortran:read inquire_id ' // dname // ' in ' // self%filename
 
-if(ier == NF90_NOERR) then
 select type (value)
 type is (character(*))
   !! NetCDF4 requires knowing the exact character length as if it were an array without fill values
   !! HDF5 is not this strict; having a longer string variable than disk variable is OK in HDF5, but not NetCDF4
-  call self%shape(dname, dims)
-  charbuf : block
-    character(len=dims(1)) :: buf
-    ier = nf90_get_var(self%ncid, varid, buf)
-    i = index(buf, c_null_char) - 1
-    if(i == -1) i = len_trim(buf)
-    value = buf(:i)
-  end block charbuf
+  ier = nf90_inquire_variable(self%ncid, varid, dimids = dimids)
+  if(check_error(ier, dname)) error stop 'ERROR:nc4fortran:read_scalar: could not get dimension IDs for: ' // dname
+
+  ier = nf90_inquire_dimension(self%ncid, dimid=dimids(1), len=dims)
+  if(ier/=NF90_NOERR) error stop 'ERROR:nc4fortran:read_scalar: querying dimension size'
+
+  allocate(character(dims) :: buf)
+  ier = nf90_get_var(self%ncid, varid, buf)
+  i = index(buf, c_null_char) - 1
+  if(i == -1) i = len_trim(buf)
+  value = buf(:i)
 type is (real(real64))
   ier = nf90_get_var(self%ncid, varid, value)
 type is (real(real32))
@@ -37,9 +41,7 @@ type is (integer(int32))
 class default
   ier = NF90_EBADTYPE
 end select
-endif
-
-if (check_error(ier, dname)) error stop 'nc4fortran:read failed ' // dname // ' in ' // self%filename
+if (check_error(ier, dname)) error stop 'ERROR:nc4fortran:read_scalar: failed ' // dname // ' in ' // self%filename
 
 end procedure nc_read_scalar
 
