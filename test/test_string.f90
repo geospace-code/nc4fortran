98c29d7a2f2425a774935b3a9dd2c2ea1686ecb4
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-29 14:22:36 -0400

    add more string tests

--- test/test_string.f90
+++ test/test_string.f90
@@ -7,36 +7,85 @@ use nc4fortran, only : netcdf_file
 
 implicit none (type, external)
 
+character(*), parameter :: fn='test_string.nc'
+
+call test_write(fn)
+print *, "OK: HDF5 string write"
+
+call test_read(fn)
+print *,'OK: HDF5 string read'
+
+call test_overwrite(fn)
+print *, "OK: string overwrite"
+
+print *,'PASSED: HDF5 string write/read'
+
+contains
+
+
+subroutine test_write(fn)
+
+character(*), intent(in) :: fn
 
 type(netcdf_file) :: h
 
-character(2) :: value
-character(1024) :: val1k
-character(*), parameter :: path='test_string.nc'
-
-call h%open(path, action='w')
+call h%open(fn, action='w')
 
 call h%write('little', '42')
 call h%write('MySentence', 'this is a little sentence.')
 
 call h%close()
 
-call h%open(path, action='r')
+end subroutine test_write
+
+
+subroutine test_read(fn)
+
+character(*), intent(in) :: fn
+
+type(netcdf_file) :: h
+character(2) :: value
+character(1024) :: val1k
+
+call h%open(fn, action='r')
 call h%read('little', value)
 
+if(len_trim(value) /= 2) then
+  write(stderr,'(a,i0,a)') "test_string: read length ", len_trim(value), " /= 2"
+  error stop
+endif
 if (value /= '42') error stop 'test_string:  read/write verification failure. Value: '// value
 
-print *,'test_string_rw: reading too much data'
-!! try reading too much data, then truncating to first C_NULL
+!> longer character than data
 call h%read('little', val1k)
 
 if (len_trim(val1k) /= 2) then
-  write(stderr, *) 'expected len_trim 2, got len_trim = ', len(val1k)
+  write(stderr, '(a,i0,/,a)') 'expected character len_trim 2 but got len_trim() = ', len_trim(val1k), val1k
   error stop
 endif
 
 call h%close()
 
-print *, 'OK: test_string'
+end subroutine test_read
+
+
+subroutine test_overwrite(fn)
+
+character(*), intent(in) :: fn
+
+type(netcdf_file) :: h
+character(2) :: v
+
+call h%open(fn, action='rw')
+call h%write('little', '73')
+call h%close()
+
+call h%open(fn, action='r')
+call h%read('little', v)
+call h%close()
+
+if (v /= '73') error stop 'test_string:  overwrite string failure. Value: '// v // " /= 73"
+
+end subroutine test_overwrite
 
 end program
