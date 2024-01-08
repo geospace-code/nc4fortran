d2d6cd63f50b535341ddab3efd48277b41d4d21a
scivision <scivision@users.noreply.github.com>
2024-01-07 20:23:00 -0500

    remove abi check

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,24 +1,3 @@
-# check C and Fortran compiler ABI compatibility
-
-if(NOT abi_ok)
-  message(CHECK_START "checking that compilers can link together")
-  try_compile(abi_ok
-  ${CMAKE_CURRENT_BINARY_DIR}/abi_check ${CMAKE_CURRENT_LIST_DIR}/abi_check
-  abi_check
-  OUTPUT_VARIABLE abi_log
-  )
-  if(abi_ok)
-    message(CHECK_PASS "OK")
-  else()
-    message(FATAL_ERROR "ABI-incompatible compilers:
-    C compiler ${CMAKE_C_COMPILER_ID} ${CMAKE_C_COMPILER_VERSION}
-    Fortran compiler ${CMAKE_Fortran_COMPILER_ID} ${CMAKE_Fortran_COMPILER_VERSION}
-    ${abi_log}
-    "
-    )
-  endif()
-endif()
-
 # --- C compile flags
 if(CMAKE_C_COMPILER_ID MATCHES "Clang|GNU|^Intel")
   add_compile_options(
